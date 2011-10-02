131f428e8ee596bd0ba2c7b0e70bcef9107544bc
vladimir.adamic@gmail.com <ovca@LinuxShtala.(none)>
2011-10-02 23:15:01 +0200

    Prvi commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opasniprojekt::Application.config.secret_token = 'dc83811edab4e08916db6710cd78fb64f3e3966b0a16fbf50d2fc5066157eb7dbaf23a20441dbb7b27ac7827c6447d0d5b6d906c9b20d46dd8ae2359365338f1'
